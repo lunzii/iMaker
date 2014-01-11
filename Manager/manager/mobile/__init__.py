c983356794b05a9ac1a27389d90e535d7ddea2cc
lunzic <lunzic@gmail.com>
2014-01-11 13:36:03 +0800

    update

---
+++ Manager/manager/mobile/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 14-1-11
+
+@author: olunxchen
+'''
---
+++ Manager/manager/mobile/views.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 14-1-11
+
+@author: olunxchen
+'''
+
+from django.shortcuts import render_to_response
+
+def index(request):
+    return render_to_response('m_index.html')
